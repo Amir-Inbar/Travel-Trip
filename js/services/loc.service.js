67f36fac3ccfbeca716ad10d5b3fad923959bd5a
zivtal <zivtal83@gmail.com>
2021-11-03 15:29:37 +0200

    minor change

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,31 +1,32 @@
-export const locService = {
-	getLocs,
-	addLocation
-};
 import { storService } from './storage.service.js';
+
+export const locService = {
+    getLocs,
+    addLocation
+};
 
 var id = 0;
 const locs = [];
 const KEY = 'saveLoc';
 
 function getLocs() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve(locs);
-		}, 2000);
-	});
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(locs);
+        }, 2000);
+    });
 }
 
 function addLocation(name, lat, lng, lastUpdateAt) {
-	const location = {
-		id: id++,
-		name,
-		lat,
-		lng,
-		weather: null,
-		createAt: Date.now(),
-		lastUpdateAt
-	};
-	locs.push(location);
-	storService.save(KEY, locs);
+    const location = {
+        id: id++,
+        name,
+        lat,
+        lng,
+        weather: null,
+        createAt: Date.now(),
+        lastUpdateAt
+    };
+    locs.push(location);
+    storService.save(KEY, locs);
 }
